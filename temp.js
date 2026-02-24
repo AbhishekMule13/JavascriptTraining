a48561a5fbfcd262dcd262d10158f74d73d4edb2
AbhishekMule13 <abhimule1308@gmail.com>
2026-02-24 09:11:04 +0530

    commit

---
+++ temp.js
@@ -0,0 +1,16 @@
+// first="abhishek";
+// last="Mule";
+// // console.log(first+" "+last);
+
+// const city="Pune";
+// const city1="mumbai";
+// console.log(city1.toUpperCase());
+
+// fruits=["apple","banana","grapes"];
+// console.log(fruits.length)
+// fruit=fruits[1]
+// console.log(fruit)
+
+
+
+
