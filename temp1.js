f337655c1e96806d533c1544e427e0e3938d257e
AbhishekMule13 <abhimule1308@gmail.com>
2026-02-24 17:11:42 +0530

    Update temp1.js

--- temp1.js
+++ temp1.js
@@ -1,10 +1,11 @@
-// let a=5;
-// a++;
-// ++a
-// a++
-// console.log(a);
+// // let a=5;
+// // a++;
+// // ++a
+// // a++
+// // console.log(a);
 
-let a=2
-a++
---a
-console.log(a)
+// let a=2
+// a++
+// --a
+// console.log(a) 
+
